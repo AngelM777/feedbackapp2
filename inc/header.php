d0ae3db7e0acf3a1bfa160dee23e3082a1117111
Admin <admin@Admins-MacBook-Pro.local>
2023-04-20 22:46:09 -0700

    could this actually work

--- inc/header.php
+++ inc/header.php
@@ -21,15 +21,15 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
          <li class="nav-item">
-              <a class="nav-link" href="/php-crash/_starter_files/feedbackapp/feedbackapp2/index.php">Home</a>
+              <a class="nav-link" href="/index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/php-crash/_starter_files/feedbackapp/feedbackapp2/feedback.php"
+              <a class="nav-link" href="/feedback.php"
                 >Feedback</a
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/php-crash/_starter_files/feedbackapp/feedbackapp2/about.php"
+              <a class="nav-link" href="/about.php"
                 >About</a
               >
             </li>
